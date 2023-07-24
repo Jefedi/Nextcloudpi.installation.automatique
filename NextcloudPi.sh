51ce970d0f75be2aa4491d4af5c948e3c7a2b867
Jefedi <92695972+Jefedi@users.noreply.github.com>
2023-07-24 17:16:21 +0200

    Add files via upload

---
+++ NextcloudPi.sh
@@ -0,0 +1,10 @@
+echo "By Jefe"
+echo "Debutant" 
+clear
+echo "By Jefe"
+echo "Debutant" 
+sudo apt-get update && sudo apt-get upgrade -y
+sudo curl -sSL https://raw.githubusercontent.com/nextcloud/nextcloudpi/master/install.sh | sudo bash -y
+clear 
+echo "By Jefe"
+echo "NextCloudPi Installer"
