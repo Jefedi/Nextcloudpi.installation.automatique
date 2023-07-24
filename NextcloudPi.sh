d8e4085c32444b9571532264f89ac7dc36cf6239
Jefedi <92695972+Jefedi@users.noreply.github.com>
2023-07-24 17:33:11 +0200

    Update NextcloudPi.sh

--- NextcloudPi.sh
+++ NextcloudPi.sh
@@ -1,12 +1,10 @@
-echo "By Jefe"
+echo "Jefe"
 echo "Debutant" 
 sudo apt-get update && sudo apt-get upgrade -y
 sudo curl -sSL https://raw.githubusercontent.com/nextcloud/nextcloudpi/master/install.sh | sudo bash
-clear 
 echo "By Jefe"
 echo "NextCloudPi Installer"
 clear
 echo "Installation de Cockpit"
 sudo apt install cockpit
 echo "Cockpit installer"
-clear
