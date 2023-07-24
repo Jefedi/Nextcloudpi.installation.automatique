7839e98033e263acfba631d4d9617f0691b92544
Jefedi <92695972+Jefedi@users.noreply.github.com>
2023-07-24 17:27:01 +0200

    Update NextcloudPi.sh

--- NextcloudPi.sh
+++ NextcloudPi.sh
@@ -1,8 +1,5 @@
 echo "By Jefe"
 echo "Debutant" 
-clear
-echo "By Jefe"
-echo "Debutant" 
 sudo apt-get update && sudo apt-get upgrade -y
 sudo curl -sSL https://raw.githubusercontent.com/nextcloud/nextcloudpi/master/install.sh | sudo bash
 clear 
